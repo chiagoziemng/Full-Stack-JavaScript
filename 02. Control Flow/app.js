e62d9f205e1816bf4fa3a5398765f95e220c7035
chiagoziemng <chiagoziemjamesng@gmail.com>
2025-06-05 22:58:25 +0100

    fest: add for loop and while loop

--- 02. Control Flow/app.js
+++ 02. Control Flow/app.js
@@ -1,23 +1,9 @@
-// Create variable name (fruit) & store "banana".
-let fruit = "";
+// Print "Your Name" 100 times to the console using while loop
+// Iteration will start from 10.
 
-switch (fruit) {
-  // if case is "Banana" print "Banana is good!"
-  case "banana":
-    console.log("Banana is good!");
-    break;
+let i = 10;
 
-  // if case is "orange" print " i am not a fan of orange."
-  case "orange":
-    console.log("I am not a fan of orange");
-    break;
-
-  // if case is "Apple" print "How you like them apples?"
-  case "Apple":
-    console.log("How you like them apples?");
-    break;
-
-  // default "I have never heard of that fruit."
-  default:
-    console.log("I have never heard of that fruit.");
+while (i <= 100) {
+  console.log("James", i);
+  i++;
 }
